411b293a7d6a105e1aef9ae753aad9244eb55af2
Cinemato <alharerekhaled@gmail.com>
2020-03-28 20:08:54 +0300

    Added mute button

---
+++ AlienDash/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] Sprite soundImage;
+    [SerializeField] Sprite mutedSoundImage;
+    [SerializeField] AudioClip selectSound;
+
+    [SerializeField] GameObject muteButton;
+
+    private void Start()
+    {
+        PlayerPrefs.SetInt("muted", 0);
+    }
+
+    private void Update()
+    {
+        if (PlayerPrefs.GetInt("muted") == 0)
+        {
+            muteButton.GetComponent<Image>().sprite = mutedSoundImage;
+            AudioListener.volume = 0;
+        }
+
+        else
+        {
+            muteButton.GetComponent<Image>().sprite = soundImage;
+            AudioListener.volume = 1;
+        }
+    }
+
+    public void toggleSound()
+    {
+        if(PlayerPrefs.GetInt("muted") == 0) 
+        {
+            PlayerPrefs.SetInt("muted", 1);
+        }
+
+        else
+        {
+            PlayerPrefs.SetInt("muted", 0);
+        }
+    }
+
+
+
+}
